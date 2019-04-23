357a98b610c8edf6f1b50546b9c9907791f5290d
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-04-23 19:40:58 -0400

    Hide the mysql username and passwors

---
+++ config/config.js
@@ -0,0 +1,42 @@
+// module.exports{
+//     "development": {
+//       "username": "root",
+//       "password": null,
+//       "database": "database_development",
+//       "host": "127.0.0.1",
+//       "dialect": "mysql"
+//     },
+//     "test": {
+//       "username": "root",
+//       "password": null,
+//       "database": "database_test",
+//       "host": "127.0.0.1",
+//       "dialect": "mysql"
+//     },
+//     "production": {
+//       "use_env_variable": "JAWSDB_URL",
+//       "dialect": "mysql"
+//     }
+//   }
+
+const dotenv = require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
