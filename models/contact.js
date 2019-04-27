2514ba62752ccf35b83c6a2c79030092bf279f5f
tomspalding <tspalding@effectual.com>
2019-04-27 14:23:20 -0400

    contact form

---
+++ models/contact.js
@@ -0,0 +1,17 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Contact = sequelize.define(
+    "Contact",
+    {
+      messageCat: { type: DataTypes.STRING, validate: { notEmpty: true } },
+      name: { type: DataTypes.STRING, validate: { notEmpty: true } },
+      email: { type: DataTypes.STRING, validate: { isEmail: true } },
+      message: { type: DataTypes.STRING, validate: { notEmpty: true } }
+    },
+    {}
+  );
+  Contact.associate = function(models) {
+    // associations can be defined here
+  };
+  return Contact;
+};
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -108,4 +108,24 @@ module.exports = function(app) {
     req.session.destroy();
     return res.redirect("/home");
   });
+
+  app.post("/contactSubmission", (req, res) => {
+    console.log("In Contact submission");
+    var messageCat = req.body.messageCat;
+    var name = req.body.name;
+    var email = req.body.email;
+    var message = req.body.message;
+    db.Contact.create({
+      messageCat: messageCat,
+      name: name,
+      email: email,
+      message: message
+    }).then(function(err, resp) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(resp);
+      }
+    });
+  });
 };
