aab8a69c02ffb7cbf62d27311e078fc440e6df03
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-04-30 19:12:57 -0400

    made changes

--- models/user.js
+++ models/user.js
@@ -1,13 +1,25 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: { type: DataTypes.STRING, validate: { notEmpty: true } },
-    lastName: { type: DataTypes.STRING, validate: { notEmpty: true } },
-    email: { type: DataTypes.STRING, validate: { isEmail: true } },
-    password: { type: DataTypes.STRING, validate: { notEmpty: true } }
-  }, {});
+  const User = sequelize.define(
+    "User",
+    {
+      firstName: { type: DataTypes.STRING, validate: { notEmpty: true } },
+      lastName: { type: DataTypes.STRING, validate: { notEmpty: true } },
+      email: { type: DataTypes.STRING, validate: { isEmail: true } },
+      password: { type: DataTypes.STRING, validate: { notEmpty: true } },
+      street: { type: DataTypes.STRING, validate: { notEmpty: true } },
+      city: { type: DataTypes.STRING, validate: { notEmpty: true } },
+      state: { type: DataTypes.STRING, validate: { notEmpty: true } },
+      zipcode: { type: DataTypes.INTEGER, validate: { notEmpty: true } }
+    },
+    {}
+  );
   User.associate = function(models) {
     // associations can be defined here
+    User.hasMany(models.Item, {
+      foreignKey: "userId",
+      onDelete: "cascade"
+    });
   };
   return User;
 };
