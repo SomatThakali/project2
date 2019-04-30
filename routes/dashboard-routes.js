09481e23f16ea954f99045692cdceab9553fc125
Tasha <natalytak@yahoo.com>
2019-04-30 17:33:54 -0400

    lending.handlebars

--- routes/dashboard-routes.js
+++ routes/dashboard-routes.js
@@ -10,7 +10,18 @@ module.exports = function(app) {
   app.delete("/api/lending", async (req, res) => {
     res.render("lending", {title: "Lending"});
   });
-  
+  app.get("/borrowing", (req, res) =>
+      db.Items.findAll().then(function(items) {
+        res.render("borrowing", {
+          name: items[0].dataValues.name,
+          name: items[0].name,
+          category: items[0].category,
+          description: items[0].description,
+          daysBorrow: items[0].borrow_days
+        })
+      })
+  );
+
   // PUT route for updating posts
   // app.put("/api/items", function(req, res) {
   //   db.Item.update(
