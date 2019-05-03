67a9c86f163505d50c58385c2ac60b399b58c755
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-05-03 19:29:08 -0400

    deleted unwanted codes

--- routes/dashboard-routes.js
+++ routes/dashboard-routes.js
@@ -132,12 +132,4 @@ module.exports = function(app) {
       }
     );
   });
-
-  // let uploading = multer({
-  //   dest: __dirname + "../public/assets/images"
-  // });
-
-  // app.post("/upload", uploading, function(req, res) {
-  //   let image = req.file;
-  // });
 };
