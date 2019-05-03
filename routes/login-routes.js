6d5fd88c3f829426e7c55cddaddf17ba481e34d2
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-05-03 00:45:03 -0400

    Fixed error message

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -8,24 +8,36 @@ module.exports = function(app) {
     res.render("login", { title: "login" })
   );
 
-  app.post("/login", function(req, res, next) {
-    // generate the authenticate method and pass the req/res
+  // app.post("/login", function(req, res, next) {
+  //   // generate the authenticate method and pass the req/res
 
-    passport.authenticate("local", function(err, user, info) {
-      if (err) {
-        return next(err);
-      }
-      if (!user) {
-        return res.redirect("/login");
-      }
-      req.login(user, function(err) {
-        if (err) {
-          return next(err);
-        }
-        res.redirect(`dashboards/${user[0].dataValues.id}`);
-      });
-    })(req, res, next);
-  });
+  //   passport.authenticate("local", function(err, user, info) {
+  //     if (err) {
+  //       return next(err);
+  //     }
+  //     if (!user) {
+  //       return res.redirect("/login");
+  //     }
+  //     req.login(user, function(err) {
+  //       if (err) {
+  //         return next(err);
+  //       }
+  //       res.redirect(`dashboards/${user[0].dataValues.id}`);
+  //     });
+  //   })(req, res, next);
+  // });
+
+  app.post(
+    "/login",
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true
+    }),
+    function(req, res) {
+      console.log("user", req.user);
+      res.redirect(`dashboards/${req.user[0].dataValues.id}`);
+    }
+  );
 
   app.get("/logout", (req, res) => {
     req.logout();
